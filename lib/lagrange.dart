b1bbfb78b2f586ea2233f18cbcee936b379664a0
Axel Christ <adracus@gmail.com>
2014-10-13 14:13:50 +0200

    Add modular lagrange interpolation, fixes

--- lib/lagrange.dart
+++ lib/lagrange.dart
@@ -29,10 +29,46 @@ Func lagrangeInt(List<Point<int>> points) {
       int totMul = 1;
       for (int j = 0; j < points.length; j++) {
         if (i == j) continue;
-        totMul *= (x - points[j].x) / (points[i].x - points[j].x);
+        totMul *= (x - points[j].x) ~/ (points[i].x - points[j].x);
       }
       total += points[i].y * totMul;
     }
     return total;
   };
+}
+
+
+Func modularLagrange(List<Point<int>> points, int prime) {
+  return (int x) {
+    int total = 0;
+    for (int i = 0; i < points.length; i++) {
+      int numerator = 1;
+      int denominator = 1;
+      for (int j = 0; j < points.length; j++) {
+        if (i == j) continue;
+        numerator = (numerator * (x - points[j].x)) % prime;
+        denominator = (denominator * (points[i].x - points[j].x)) % prime;
+      }
+      int lagrangePolynomial = numerator * _modInverse(denominator, prime);
+      total = (prime + total + points[i].y * lagrangePolynomial) % prime;
+    }
+    return total;
+  };
+}
+
+
+int _modInverse(int k, int prime) {
+  int v = k % prime;
+  int r = _egcd(prime, v.abs())[2];
+  return (prime + r) % prime;
+}
+
+
+List<int> _egcd(int a, int b) {
+  if (a == 0) return [b, 0, 1];
+  var res = _egcd(b % a, a);
+  var g = res[0];
+  var y = res[1];
+  var x = res[2];
+  return [g, x - (b / a).floor() * y, y];
 }
