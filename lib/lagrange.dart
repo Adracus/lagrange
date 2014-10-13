c791fb8a453ddbebc594365e0a2aeb1cdc977968
Axel Christ <adracus@gmail.com>
2014-10-13 13:58:45 +0200

    Add lagrange for big ints

--- lib/lagrange.dart
+++ lib/lagrange.dart
@@ -20,3 +20,19 @@ Func lagrange(List<Point<num>> points) {
     return total;
   };
 }
+
+
+Func lagrangeInt(List<Point<int>> points) {
+  return (int x) {
+    int total = 0;
+    for (int i = 0; i < points.length; i++) {
+      int totMul = 1;
+      for (int j = 0; j < points.length; j++) {
+        if (i == j) continue;
+        totMul *= (x - points[j].x) / (points[i].x - points[j].x);
+      }
+      total += points[i].y * totMul;
+    }
+    return total;
+  };
+}
